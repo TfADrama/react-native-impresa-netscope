01777ee70bdab5cff3c75e56eed076f215fbde33
Tiago Freitas <tfreitas@impresa.pt>
2020-01-26 17:18:25 +0000

    removed setup tracker

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-import { NativeModules } from 'react-native';
+import { NativeModules } from "react-native";
 
 function Netscope() {
   const { setupTracker, callAnalytics } = NativeModules.RNImpresaNetscope;
 
-  const setup = () => {
-    setupTracker(
-      'aaaa',
-      '12.0',
-      'https://gapt.hit.gemius.pl',
-      'aaaaa'
-    );
-  };
-
-  const sendAnalytics = message => {
-    callAnalytics(message);
-  };
-
   return {
-    setup,
-    sendAnalytics,
+    setupTracker: (appInfo, version, hitCollectorHost, scriptIdentifier) => {
+      setupTracker(appInfo, version, hitCollectorHost, scriptIdentifier);
+    },
+    sendAnalytics: message => {
+      callAnalytics(message);
+    }
   };
 }
 
